5e090bc23b37c74fb7524e2778525f47fba2c88d
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-11-28 14:32:40 -0600

    creation of Player class

---
+++ Player.h
@@ -0,0 +1,18 @@
+//
+//  Player.h
+//  QueueAlpha
+//
+//  Created by Joshua Hall & Kalon Stephen  on 11/28/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@property NSString *firstName;
+@property NSString *lastName;
+@property BOOL *isHoldingLine;
+@property NSInteger *points;
+
+@end
---
+++ Player.m
@@ -0,0 +1,16 @@
+//
+//  Player.m
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 11/28/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+
+
+
+@end
