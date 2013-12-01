be5ed4ee22c4560e8ff532fda5d6beaac6d4d11b
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-01 16:12:20 -0600

    created Queue model for running app

---
+++ QueueModel.h
@@ -0,0 +1,13 @@
+//
+//  QueueModel.h
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 12/1/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QueueModel : NSObject
+
+@end
---
+++ QueueModel.m
@@ -0,0 +1,13 @@
+//
+//  QueueModel.m
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 12/1/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import "QueueModel.h"
+
+@implementation QueueModel
+
+@end
