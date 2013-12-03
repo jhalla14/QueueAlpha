c432660379a305c4f85a143b43a7363717766493
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-03 12:02:08 -0600

    resolved Thread 1: SIGAbort error

--- QueueAlpha/FirstViewController.m
+++ QueueAlpha/FirstViewController.m
@@ -15,28 +15,28 @@
 
 @property (nonatomic, strong) QueueModel *model;
 
-@property (weak, nonatomic) IBOutlet UITextView *body;
-@property (weak, nonatomic) IBOutlet UILabel *headline;
-@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
+//@property (weak, nonatomic) IBOutlet UITextView *body;
+//@property (weak, nonatomic) IBOutlet UILabel *headline;
+//@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
 @implementation FirstViewController
-- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
-{
-    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
-                                  value:sender.backgroundColor
-                                  range:self.body.selectedRange];
-}
-- (IBAction)outlineBodySelection:(UIButton *)sender
-{
-    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName:@-3, NSStrokeColorAttributeName:[UIColor blackColor]} range:self.body.selectedRange];
-}
-- (IBAction)unoutlineBodySelection:(UIButton *)sender
-{
-    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
-                                     range:self.body.selectedRange];
-}
+//- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
+//{
+//    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+//                                  value:sender.backgroundColor
+//                                  range:self.body.selectedRange];
+//}
+//- (IBAction)outlineBodySelection:(UIButton *)sender
+//{
+//    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName:@-3, NSStrokeColorAttributeName:[UIColor blackColor]} range:self.body.selectedRange];
+//}
+//- (IBAction)unoutlineBodySelection:(UIButton *)sender
+//{
+//    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+//                                     range:self.body.selectedRange];
+//}
 
 
 - (QueueModel *) model
@@ -70,11 +70,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
-    
-    [title setAttributes:@{NSStrokeWidthAttributeName: @3, NSStrokeColorAttributeName:self.outlineButton.tintColor} range:NSMakeRange(0, [title length])];
-    
-    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+//    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+//    
+//    [title setAttributes:@{NSStrokeWidthAttributeName: @3, NSStrokeColorAttributeName:self.outlineButton.tintColor} range:NSMakeRange(0, [title length])];
+//    
+//    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
     //initilization and outlet setting
     //only called once in lifetime of controller
     //called before going on screen
@@ -95,15 +95,15 @@
 - (void) viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [self usePreferredFonts];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontsChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
-    
+//    [self usePreferredFonts];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontsChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+//    
     //guarented that view will appear
 }
 
 - (void) preferredFontsChanged: (NSNotification *) notification
 {
-    [self usePreferredFonts];
+//    [self usePreferredFonts];
 
 }
 
@@ -122,8 +122,8 @@
 
 - (void) viewWillDisappear:(BOOL)animated
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
-    //remember what's going on and cleanup code
+//    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+//    //remember what's going on and cleanup code
     //rememberScroolPosition
 }
 
