0a3c7aed5d1e4697184b8d80af69543fc0a68cc8
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-03 12:12:04 -0600

    created new ViewControllers for startup page

---
+++ QueueAlpha/StartUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartUpViewController.h
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 12/3/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartUpViewController : UIViewController
+
+@end
