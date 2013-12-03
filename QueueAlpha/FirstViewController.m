587e664b5bcfc70ac1a7da5cdd8efc370bb2166b
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-02 18:05:27 -0600

    playing around with some buttons and the view controllor lifecycle

--- QueueAlpha/FirstViewController.m
+++ QueueAlpha/FirstViewController.m
@@ -15,9 +15,28 @@
 
 @property (nonatomic, strong) QueueModel *model;
 
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+
 @end
 
 @implementation FirstViewController
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+- (IBAction)outlineBodySelection:(UIButton *)sender
+{
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName:@-3, NSStrokeColorAttributeName:[UIColor blackColor]} range:self.body.selectedRange];
+}
+- (IBAction)unoutlineBodySelection:(UIButton *)sender
+{
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
+
 
 - (QueueModel *) model
 {
@@ -50,12 +69,47 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //initilization and outlet setting
+    //only called once in lifetime of controller
+    //called before going on screen
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    //called just before view goes on screen
+    //called multiple times (everytime view is on screen)
+    //change of views based on data (things that changed before being visible)
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    
+}
+
+- (void) viewWillLayoutSubviews
+{
+    //called right before ios layouts views
+    //put geometry stuff here
+}
+
+- (void) viewDidLayoutSubviews
+{
+    //called after viewWillLayoutSubviews
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    //remember what's going on and cleanup code
+    //rememberScroolPosition
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    //try to free up memory in the heap
+    //set strong pointers to nil
 }
 
 @end
