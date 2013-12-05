ac28f51acff416216cd4efb32ae945a1342eaf25
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-05 12:19:43 -0600

    replaced older queueVC files with new ones

---
+++ QueueAlpha/QueuesViewController.h
@@ -0,0 +1,13 @@
+//
+//  QueuesViewController.h
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 12/5/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QueuesViewController : UIViewController
+
+@end
---
+++ QueueAlpha/QueuesViewController.m
@@ -0,0 +1,38 @@
+//
+//  QueuesViewController.m
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 12/5/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import "QueuesViewController.h"
+
+@interface QueuesViewController ()
+
+@end
+
+@implementation QueuesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
