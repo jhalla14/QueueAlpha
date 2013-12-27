cdd28d3a74eec54fc73017a2d19ed29977bf6ef2
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-12-27 14:09:07 -0600

    created new tables view

---
+++ QueueAlpha/QueuesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QueuesTableViewController.h
+//  QueueAlpha
+//
+//  Created by Joshua Hall  on 12/27/13.
+//  Copyright (c) 2013 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QueuesTableViewController : UITableViewController
+
+@end
