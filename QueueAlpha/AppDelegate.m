229e4fa7e4cbdebe632a378f7d1f28b3fa981397
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2014-01-05 09:56:01 -0500

    adding some facebook login code

--- QueueAlpha/AppDelegate.m
+++ QueueAlpha/AppDelegate.m
@@ -11,6 +11,11 @@
 
 @implementation AppDelegate
 
+- (BOOL) application:(UIApplication *) application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
@@ -42,6 +47,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
